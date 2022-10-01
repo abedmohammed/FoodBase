84c2cdddaca440ad37bab89801cab52e4f71df6c
abedm <abedmohammed353@gmail.com>
2022-10-01 09:09:39 -0400

    Helpers and configuration files

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,20 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeViews.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
